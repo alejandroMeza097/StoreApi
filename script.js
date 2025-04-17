ad9846d0b7395325b1d14d2a32aabf1cd9e6e025
alex meza <alex.meza.5471@gmail.com>
2025-04-17 12:31:07 -0600

    modificaciones para mejorar las dimensiones de las imagenes

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const main = document.getElementsByTagName("main").item(0);
 const URLMain = "https://fakestoreapi.com/products/";
 
 
-
+//FUNCION PARA OBTENER DATOS DESDE LA API...
 function getData(){
     const options = {"method":"GET"};
     fetch(URLMain,options)
@@ -22,16 +22,19 @@ function getData(){
     });
 }
 
+
+//FUNCION QUE GENERA LOS CARDS CON LA INFORMACION...
 function createCards(productos){
     productos.forEach(producto => {
         main.insertAdjacentHTML("beforeend",
             `
             <div class="card m-3" style="width: 18rem;">
-                <img class="card-img-top" src="${producto.image}" alt="Card image cap">
+                <img class="card-img-top" src="${producto.image}" alt="Card image cap" style="width:200px; height:250px">
                  <div class="card-body">
                      <h5 class="card-title">${producto.title}</h5>
                         <p class="card-text">${producto.description}</p>
                         <p class = "card-text">${producto.category}</p>
+                        <p class = "card-text text-danger"> ${producto.rating.rate}</p>
                         <a href="#" class="btn btn-primary">Go somewhere</a>
                 </div>
             </div>
